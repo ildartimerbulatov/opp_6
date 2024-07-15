0f849ba0b4857e1440f1869ed44511c42f8ddfa3
Ildar <ildartimerbulatov@yandex.ru>
2024-07-15 23:36:53 +0300

    Удалены файлы из директории src

--- FamilyTreeStorage.java
+++
@@ -1,6 +0,0 @@
-import java.io.IOException;
-
-public interface FamilyTreeStorage {
-    void saveFamilyTree(FamilyTree familyTree, String filePath) throws IOException;
-    FamilyTree loadFamilyTree(String filePath) throws IOException, ClassNotFoundException;
-}
