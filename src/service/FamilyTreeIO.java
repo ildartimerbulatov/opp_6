3ea14aff65982547b9c2606c9b58ce9d410040f9
Ildar <ildartimerbulatov@yandex.ru>
2024-07-16 11:50:26 +0300

    Добавлены файлы в директорию src

--- src/service/FamilyTreeIO.java
+++ src/service/FamilyTreeIO.java
@@ -1,20 +1,20 @@
-package service;
-
 import java.io.*;
-import model.FamilyTree;
+import java.nio.charset.StandardCharsets;
 
 public class FamilyTreeIO implements FamilyTreeStorage {
     @Override
-    public void saveFamilyTree(FamilyTree<?> familyTree, String filePath) throws IOException {
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filePath))) {
+    public void saveFamilyTree(FamilyTree familyTree, String filePath) throws IOException {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filePath));
+             OutputStreamWriter writer = new OutputStreamWriter(out, StandardCharsets.UTF_8)) {
             out.writeObject(familyTree);
         }
     }
 
     @Override
-    public FamilyTree<?> loadFamilyTree(String filePath) throws IOException, ClassNotFoundException {
+    public FamilyTree loadFamilyTree(String filePath) throws IOException, ClassNotFoundException {
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filePath))) {
-            return (FamilyTree<?>) in.readObject();
+            return (FamilyTree) in.readObject();
         }
     }
 }
+
